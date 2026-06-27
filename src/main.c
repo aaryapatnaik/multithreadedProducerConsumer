4688ead0026e2ffcdc4d30aa7d3e88217792ed9f
aaryapatnaik <66499097+aaryapatnaik@users.noreply.github.com>
2026-06-26 22:21:06 -0700

    Implemented producer and consumer thread routines

--- src/main.c
+++ src/main.c
@@ -39,6 +39,58 @@ void destroyBuffer(void)
     pthread_cond_destroy(&sharedBuffer.notEmpty);
 }
 
+void *producerThread(void *argument)
+{
+    int producerId = *(int *)argument;
+
+    for (int i = 0; i < ITEMS_PER_PRODUCER; i++) {
+        int item = producerId * ITEMS_PER_PRODUCER + i;
+
+        pthread_mutex_lock(&sharedBuffer.mutex);
+
+        while (sharedBuffer.itemCount == BUFFER_SIZE) {
+            pthread_cond_wait(&sharedBuffer.notFull, &sharedBuffer.mutex);
+        }
+
+        sharedBuffer.values[sharedBuffer.writeIndex] = item;
+        sharedBuffer.writeIndex = (sharedBuffer.writeIndex + 1) % BUFFER_SIZE;
+        sharedBuffer.itemCount++;
+
+        pthread_cond_signal(&sharedBuffer.notEmpty);
+        pthread_mutex_unlock(&sharedBuffer.mutex);
+    }
+
+    return NULL;
+}
+
+void *consumerThread(void *argument)
+{
+    int consumerId = *(int *)argument;
+    int consumedCount = 0;
+
+    (void)consumerId;
+
+    while (consumedCount < ITEMS_PER_PRODUCER) {
+        pthread_mutex_lock(&sharedBuffer.mutex);
+
+        while (sharedBuffer.itemCount == 0) {
+            pthread_cond_wait(&sharedBuffer.notEmpty, &sharedBuffer.mutex);
+        }
+
+        int item = sharedBuffer.values[sharedBuffer.readIndex];
+        sharedBuffer.readIndex = (sharedBuffer.readIndex + 1) % BUFFER_SIZE;
+        sharedBuffer.itemCount--;
+
+        pthread_cond_signal(&sharedBuffer.notFull);
+        pthread_mutex_unlock(&sharedBuffer.mutex);
+
+        consumedCount++;
+        (void)item;
+    }
+
+    return NULL;
+}
+
 int main(void)
 {
     initializeBuffer();
@@ -52,6 +104,6 @@ int main(void)
     printf("Items per producer: %d\n\n", ITEMS_PER_PRODUCER);
 
     destroyBuffer();
-    
+
     return 0;
 }
